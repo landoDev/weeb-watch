030bee5d54fee0538dc97bf44fd0724ee45c1348
Landon Turner <lturner@LTurner-MBP.lan1>
2020-02-20 09:13:16 -0500

    changed text as reminder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Future home to the Weeb Watch app
         </p>
         <a
           className="App-link"
