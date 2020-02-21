4883fe788d9f529e0293c56f120dd2f17ad4796a
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-21 17:20:38 -0500

    successful data calls, now have top fifty anime

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Header from "./components/Header";
 import './App.css';
+import axios from 'axios';
 // ADD BOOTSTRAP AND REACT STRAP TO GET THIS GOING FAST LANDO
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(()=>{
+    axios.get("https://api.jikan.moe/v3/top/anime")
+    .then(response =>{
+      console.log(response.data.top);
+      setData(response.data.top);
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Future home to the Weeb Watch app
-        </p>
-
-      </header>
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-    
-    h1{
-
+    background-color: #13366c;
+    border: 5px solid  #2a504b;
+    margin: 0 auto;
+    width: 50%;
+    h1, p{
+        color: #298acd;
+        font-family: 'Shojumaru' ;
+        text-shadow: 2px 2px black;
     }
 `;
 
 export default function Header(){
+    return(
+        <HeaderDiv>
+            <h1>Weeb Watch</h1>
+            <p>Find your next binge</p>
+        </HeaderDiv>
+    )
+
 
 }
